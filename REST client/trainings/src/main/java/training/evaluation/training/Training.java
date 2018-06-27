775a3a229f1b7617e270b93281558aa1b00b8e52
Sofija Petroska <sofija_777@hotmail.com>
2018-06-27 13:46:27 +0200

    [#3] MongoDB support in Spring Boot added

--- REST client/trainings/src/main/java/training/evaluation/training/Training.java
+++ REST client/trainings/src/main/java/training/evaluation/training/Training.java
@@ -1,5 +1,63 @@
 package training.evaluation.training;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "trainings")
 public class Training {
+    @Id
+    private String id;
+    private String name;
+    private String level;
+    private String descriptipon;
+
+    public Training() {}
+
+    public Training(String name, String level, String descriptipon) {
+        this.name = name;
+        this.level = level;
+        this.descriptipon = descriptipon;
+    }
+
+    public Training(String name, String level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getDescriptipon() {
+        return descriptipon;
+    }
+
+    public void setDescriptipon(String descriptipon) {
+        this.descriptipon = descriptipon;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Level: " + level + ", Description: " + descriptipon;
+    }
 }
--- REST client/trainings/src/main/java/training/evaluation/training/TrainingApplication.java
+++ REST client/trainings/src/main/java/training/evaluation/training/TrainingApplication.java
@@ -1,13 +1,48 @@
 package training.evaluation.training;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrainingApplication {
+public class TrainingApplication implements CommandLineRunner {
+
+
+    @Autowired
+    private TrainingRepository repository;
 
     public static void main(String[] args) {
-
         SpringApplication.run(TrainingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args){
+
+        repository.deleteAll();
+
+        // save a couple of customers
+        repository.save(new Training("Training 1", "SE"));
+        repository.save(new Training("TR2", "SSE"));
+
+        // fetch all customers
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (Training customer : repository.findAll()) {
+            System.out.println(customer);
+        }
+        System.out.println();
+
+        // fetch an individual customer
+        System.out.println("Customer found with findByName('Training 1'):");
+        System.out.println("--------------------------------");
+        System.out.println(repository.findByName("Training 1"));
+
+        System.out.println("Customers found with findByLevel('SSE'):");
+        System.out.println("--------------------------------");
+        for (Training training : repository.findByLevel("SSE")) {
+            System.out.println(training);
+        }
+
     }
 }
