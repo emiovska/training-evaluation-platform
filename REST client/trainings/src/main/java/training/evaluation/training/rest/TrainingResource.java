de57788dfbe03a09b43e4ecda33518c68049882d
goran.petrovski <goki.petrovski@gmail.com>
2018-06-28 11:08:13 +0200

    [#19] Set swagger annotation for create endpoint

--- REST client/trainings/src/main/java/training/evaluation/training/rest/TrainingResource.java
+++ REST client/trainings/src/main/java/training/evaluation/training/rest/TrainingResource.java
@@ -1,5 +1,8 @@
 package training.evaluation.training.rest;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +16,15 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(description = "Resource to expose all available training endpoints", tags = {"TrainingResource"})
 public class TrainingResource {
 
     @Autowired
     private TrainingRepository repository;
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public Training createTraining(@Valid @RequestBody Training training) {
+    @ApiOperation(value = "Create new training", notes = "The training must contain name and level property")
+    public Training createTraining(@ApiParam(value = "Training object", required = true) @Valid @RequestBody Training training) {
         repository.save(training);
         return training;
     }
