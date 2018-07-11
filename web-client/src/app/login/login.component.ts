0e179dfd87e4efd27e6c75ea5e167ba62e699e9f
goran.petrovski <goki.petrovski@gmail.com>
2018-07-11 15:26:43 +0200

    make design improvement on background of app. and login component page

--- web-client/src/app/login/login.component.ts
+++ web-client/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, NgForm, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import { first } from '../../../node_modules/rxjs/operators';
@@ -11,11 +11,10 @@ import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
   returnUrl: string;
-  submitted = false;
-  loading = false;
+  submitting = false;
 
+  //for snack bar toast notification position
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
 
@@ -27,41 +26,32 @@ export class LoginComponent implements OnInit {
     public snackBar: MatSnackBar) { }
 
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-
     this.authenticationService.logout();
-
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  get f() {
-    return this.loginForm.controls;
+  onSubmit(signInForm: NgForm) {
+    if (signInForm.valid) {
+      this.submitting = true;
+
+      this.authenticationService.login(signInForm.value.username, signInForm.value.password)
+        .pipe(first()).subscribe(
+          data => {
+            console.log('got valid: ', data);
+            this.router.navigate(['/authenticated']);
+          }, (err) => {
+            this.submitting = false;
+            this.showNotification("Incorect username or password !!!");
+          }
+        );
+    }
   }
 
-  onSubmit() {
-    this.submitted = true;
-    console.log("Submit login", this.f.username.value, "pass");
-    //this.loading = true;
-    this.authenticationService.login(this.f.username.value, this.f.password.value)
-      .pipe(first()).subscribe(
-        data => {
-          this.router.navigate([this.returnUrl]);
-        },
-        error => {
-          this.snackBar.open("Incorect username or password !!!", "Close", {
-            duration: 5000,
-            horizontalPosition: this.horizontalPosition,
-            verticalPosition: this.verticalPosition,
-          });
-          //this.loading = true;
-        });
+  showNotification(message: string) {
+    this.snackBar.open(message, "Close", {
+      duration: 5000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
   }
 }
-
-
-
-
-// }
