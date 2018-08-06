699eaf9e0744a09843fc21ef0acf6e721f6b2762
Sofija Petroska <sofija_777@hotmail.com>
2018-08-06 10:56:23 +0200

    #91 New fields in Training model added (picture, skills, trainer)

--- REST client/trainings/src/main/java/training/evaluation/training/model/Training.java
+++ REST client/trainings/src/main/java/training/evaluation/training/model/Training.java
@@ -1,7 +1,10 @@
 package training.evaluation.training.model;
 
+import org.bson.types.Binary;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Document(collection = "trainings")
 public class Training {
@@ -11,12 +14,22 @@ public class Training {
     private String level;
     private String description;
 
-    public Training() {}
+    private Binary imageFile;
 
-    public Training(String name, String level, String description) {
+    private String trainer;
+    private List<String> skills;
+
+    public Training() {
+    }
+
+    public Training(String id, String name, String level, String description, Binary imageFile, String trainer, List<String> skills) {
+        this.id = id;
         this.name = name;
         this.level = level;
         this.description = description;
+        this.imageFile = imageFile;
+        this.trainer = trainer;
+        this.skills = skills;
     }
 
     public Training(String name, String level) {
@@ -54,6 +67,30 @@ public class Training {
 
     public void setDescription(String descriptipon) {
         this.description = descriptipon;
+    }
+
+    public Binary getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(Binary imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public String getTrainer() {
+        return trainer;
+    }
+
+    public void setTrainer(String trainer) {
+        this.trainer = trainer;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
     }
 
     @Override
