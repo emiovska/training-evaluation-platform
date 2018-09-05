d4dc4304361f7e4e8338c6e7d637f29b32e56e74
Sofija Petroska <sofija_777@hotmail.com>
2018-09-05 14:59:21 +0200

    #161 REST service to get all approved Training Resquests exposed

--- REST client/trainings/src/main/java/training/evaluation/training/repository/TrainingRequestRepository.java
+++ REST client/trainings/src/main/java/training/evaluation/training/repository/TrainingRequestRepository.java
@@ -3,9 +3,11 @@ package training.evaluation.training.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import training.evaluation.training.model.TrainingRequest;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface TrainingRequestRepository extends MongoRepository<TrainingRequest, String> {
     Optional<TrainingRequest> findById(String id);
+    List<TrainingRequest> findByStatus(String status);
 }
